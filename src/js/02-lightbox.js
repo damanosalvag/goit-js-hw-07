05f2f8e7b6d1b461546e80bec7820edd1c34f891
Dairo Manosalva <38769711+damanosalvag@users.noreply.github.com>
2023-04-05 15:54:35 -0500

    task 2 module 7

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const containerPics = document.querySelector('.gallery');
+
+const elementsGallery = galleryItems.map(pic => {
+    const li = `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${pic.original}">
+            <img
+                class="gallery__image"
+                src='${pic.preview}'
+                alt='${pic.description}'
+            />
+        </a>
+    </li>`;
+    return li;
+}).join(' ');
+
+containerPics.insertAdjacentHTML('afterbegin', elementsGallery);
+
+const instancia = new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData: 'alt'});
+
